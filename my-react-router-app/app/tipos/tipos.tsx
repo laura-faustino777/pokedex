07adb14854d47661854681f40ef8ad18eee72400
Ana Laura <laurafaustino876@gmail.com>
2026-05-29 13:03:12 -0300

    feat: :sparkles:
    ATIVIDADE DE TYPES MUDAR P/

---
+++ my-react-router-app/app/tipos/tipos.tsx
@@ -0,0 +1,41 @@
+export function Tipos() {
+  return (
+    <main>
+       <div className="bg-red-700 flex justify-beetwen ">
+        <h1 className= "p-5 flex text-white text-8 font-bold">POKEDEX</h1>
+
+            <div className=" p-5 flex gap-4 cursor point  text-white font-bold align-text-top">
+                <h1>Home</h1>
+                <h1>Pokémons</h1>
+                <h1>Tipos</h1>
+            </div>
+         </div>
+         
+        
+
+        <div className= "mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10 text-center  ">
+        
+        <div>
+        <h1 className="font-bold "> Tipos de Pokémon </h1>
+        <h1 className="font-serif ">Conheça os principais tipos e suas caracteristicas</h1>
+        </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </main>
+  );
+}
